3e0793154250e0136047428b7ac440dcd72f53d2
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-05-15 10:55:28 +0200

    J'ai changer le nom des variable

--- projet_liste_des_course/liste_des_course.py
+++ projet_liste_des_course/liste_des_course.py
@@ -1,56 +1,60 @@
 
-tabIngredient = []
-nbIngredient = 0
+tab_ingredient = []
+nb_ingredient = 0
 
 print("Tu dois taper : ")
 print(" 1 - ajouter un ingredient")
 print(" 2- supprimer un ingredient")
 print("3 quitter l'application")
 
-nbSaisie = int(input("Quel est ton nb"))
+nb_saisie = int(input("Quel est ton nb"))
 # EN CAS D'ERREUR DE SAISIE 
-while nbSaisie!= 1 and nbSaisie!=2 and nbSaisie!=3 : 
+while nb_saisie!= 1 and nb_saisie!=2 and nb_saisie!=3 : 
     print("erreur")
     print("1-Pour ajouter un ingredient")
     print("2 pour supprimer un ingredient")
     print("3 pour quitter ")
-    nbSaisie = int(input("Quel est ton nb"))
+    nb_saisie = int(input("Quel est ton nb "))
 
 # AJOUTER UN INGREDIENT
-while nbSaisie == 1:
+while nb_saisie == 1:
     ingredient = str(input("Saisie ton ingredient ")).lower().strip()
-    tabIngredient.append(ingredient)
-    nbIngredient =  nbIngredient+1
-    #Cela permet d'inserer des nombre 
-    if tabIngredient !=0 :
+    tab_ingredient.append(ingredient)
+    nb_ingredient =  nb_ingredient+1
+
+    #Cela permet d'inserer un nombre
+    if tab_ingredient !=0 :
          print("1 - ajouter un nouvelle ingredient") 
          print("2 supprimer")
          print("3 - Quitter")
-         nbSaisie = int(input("Quel est ton nb"))
+         nb_saisie = int(input("Quel est ton nb "))
 
 # Supprimer un ingredient 
-while nbSaisie == 2:
+while nb_saisie == 2:
+
     #aucun ingredient 
-    if nbIngredient == 0:
+    if nb_ingredient == 0:
           print("Si tu veux supprimer un ingredient il faut que tu en ai un ");
 
           ingredient = str(input("Saisie ton ingredient ")).lower().strip()
-          tabIngredient.append(ingredient)
-          nbIngredient =  nbIngredient+1
+          tab_ingredient.append(ingredient)
+          nb_ingredient =  nb_ingredient+1
 
-    print(tabIngredient)
-    print(nbIngredient)
+    print(tab_ingredient)
+    print(nb_ingredient)
+    #
+    ingredient_supprimer = str(input("Ingredient à supprimer ")).lower().strip()
 
-    
-    for item in tabIngredient:
+    #boucle pour voir chaque ingredient 
+    for item in tab_ingredient:
          print("mes ingredient",item)
         
     
     #Permet a l'utilisateur de choisir le nb 
-    if tabIngredient !=0 :
-         nbSaisie = int(input("Quel est ton nb"))
+    if tab_ingredient !=0 :
+         nb_saisie = int(input("Quel est ton nb "))
     
 
 
 
-print("Ma liste d'ingredient ", tabIngredient)
+print("Ma liste d'ingredient ", nb_ingredient)
