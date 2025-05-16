e49ac60a51422db895cf72ec1bcc6c375e0767b6
Tatiana-Juin <username@users.noreply.github.com>
2025-05-17 01:56:27 +0200

    J'ai recuperer l'ancienne versionb . La suprresion fonctionne . J'ai un fichier liste_des_course_test pour faire des test sans toucher le code

--- projet_liste_des_course/liste_course_test.py
+++ projet_liste_des_course/liste_course_test.py
@@ -1,10 +1,7 @@
-
 tab_ingredient = []
-#pour trouver index de l'element du tableau
+nb_ingredient = 0
 nb_iteration = 0;
-#pour savoir si l'ingredient a etait trouver 
 ingredient_trouver = False;
-
 
 print("Tu dois taper : ")
 print(" 1 - ajouter un ingredient")
@@ -23,21 +20,11 @@ while nb_saisie!= 1 and nb_saisie!=2 and nb_saisie!=3 :
 # AJOUTER UN INGREDIENT
 while nb_saisie == 1:
     ingredient = str(input("Saisie ton ingredient ")).lower().strip()
-  
-    #cela ajoute obligatoirement l'element 
+    tab_ingredient.append(ingredient)
+    nb_ingredient =  nb_ingredient+1
 
-    #Si tab_ingredient n'a aucun element  : on met not car dans une condition tab_ingredient sans not est egale a True c'est pour cela qu'on met not 
-    if not tab_ingredient:
-         tab_ingredient.append(ingredient)
-
-    elif ingredient in tab_ingredient :
-         print("Tu as deja inserer cette ingredient ")
-    else:
-        tab_ingredient.append(ingredient)
-        print("Ingrédient inséré.")
-
-    #Cela permet d'inserer un nombre Superieur a 0 : si la liste d'ingredient n'est pas vide 
-    if tab_ingredient :
+    #Cela permet d'inserer un nombre
+    if tab_ingredient !=0 :
          print("1 - ajouter un nouvelle ingredient") 
          print("2 supprimer")
          print("3 - Quitter")
@@ -54,34 +41,38 @@ while nb_saisie == 2:
           tab_ingredient.append(ingredient)
           nb_ingredient =  nb_ingredient+1
 
-
-    
+#     print(tab_ingredient)
+#     print(nb_ingredient)
+    #
     ingredient_supprimer = str(input("Ingredient à supprimer ")).lower().strip()
 
     #boucle pour voir chaque ingredient 
     for item in tab_ingredient:
-     
+     #     print("mes ingredient",item)
          nb_iteration +=1
-         
+
          if item == ingredient_supprimer:
+              print("L'ingredient existe")
               nb_iteration= nb_iteration -1
-          
+              print("Le nb iteration",nb_iteration)
+          #     print("Le nb iteration",nb_iteration)
               tab_ingredient.pop(nb_iteration)
               #renitialisation de la variable
               nb_iteration = 0
               ingredient_trouver = True;
               break
-         
+
     # CONDITION AU CAS OU L'INGREDIENT EXITE PAS  
     if ingredient_trouver == False:
          print("L'ingredient existe pas") 
-     
-    
+
+
     #Permet a l'utilisateur de choisir le nb 
-    if tab_ingredient  :
+    if tab_ingredient !=0 :
          nb_saisie = int(input("Quel est ton nb "))
-    
 
 
 
+
+print("nombre iteration ", nb_iteration)
 print("Ingredient  ", tab_ingredient)
