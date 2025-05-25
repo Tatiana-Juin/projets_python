f95889a1d1f51e6e87ba1a5b496457eeb964f2e5
Tatiana-Juin <username@users.noreply.github.com>
2025-05-25 12:39:28 +0200

    Creation du projet nb_aleatoire qui fonctionne

---
+++ nb_aleatoire/version_ameliorer.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+import random
+
+nb_vie = 5
+
+#randint utiliser pour generer un nb aleatoire qui est un entier 
+nb_aleatoire = random.randint(1, 10)
+print(nb_aleatoire)
+
+nb_utilisateur = int(input("entrer un nombre entre 1 et 10 ? "))
