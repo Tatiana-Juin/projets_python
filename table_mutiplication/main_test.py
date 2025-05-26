ac2897ad3f57910b182dab3763a1de26c7f462fe
Tatiana-Juin <username@users.noreply.github.com>
2025-05-27 00:53:05 +0200

    J'ai realiser le projet qui a pour but de creer des table de multiplication

--- table_mutiplication/main_test.py
+++ table_mutiplication/main_test.py
@@ -1,1 +1,6 @@
 
+nb_multiplier = int(input("Quel est le nombre a multiplier "))
+
+for i in range (1,11):
+   resultat = i * nb_multiplier
+   print(f" {i} * {nb_multiplier} = {resultat}")
---
+++ table_mutiplication/reflexion.md
@@ -0,0 +1,3 @@
+# reflexion 
+- demande a l'utilisateur de saisir un nb 
+- boucle et multiplie 
