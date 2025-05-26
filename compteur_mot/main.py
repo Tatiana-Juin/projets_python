37a2b32f4c4c9911e761d9c7881dccc9b03618e1
Tatiana-Juin <username@users.noreply.github.com>
2025-05-27 00:26:25 +0200

    j'ai reussi a creer un programme qui compte le nb de mot

--- compteur_mot/main.py
+++ compteur_mot/main.py
@@ -1,1 +1,13 @@
 
+
+#demande a l'utilisateur de saisir du texte 
+texte_utilisateur = str(input("Saisie ton texte : ")).strip()
+
+#convertir la liste de caractere en mot 
+mots = texte_utilisateur.split()
+# variable pour connaitre le nb de mot 
+nb_mot = len(mots)
+#affiche le nombre de mot 
+print(f"le nombre de mot est de {nb_mot}")
+
+
