c3a85b74a01ebc4d95a1ca10cb3ace8b7ff9ecef
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-06-08 20:35:21 +0200

    J'ai fini le projet compte a rebourt

--- compteur_mot_et_caractere/main.py
+++ compteur_mot_et_caractere/main.py
@@ -1,18 +1,38 @@
-#demande a l'utilisateur de saisir du texte 
-texte_utilisateur = str(input("Saisie ton texte : ")).strip()
-
-#convertir la liste de caractere en mot 
-mots = texte_utilisateur.split()
-# variable pour connaitre le nb de mot 
-nb_mot = len(mots)
-#affiche le nombre de mot 
-print(f"le nombre de mot est de {nb_mot}")
-
-#pour separer chaque caratere d'une chaine de texte 
-caractere_seule= list(texte_utilisateur)
-nb_caractere = len(caractere_seule)
-print(f"Le nombre de caractere est de {nb_caractere}")
 
 
 
 
+
+
+
+
+
+import time
+import os
+
+def clear_console():
+    # Commande pour effacer la console (dépend du système d'exploitation)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# VREFICATION QUE L'UTILISATEUR N'A PAS SAISIE AUTRE CHOSE QUE UN NOMBRE  
+
+try:
+    seconde_saisie = int(input("Saisie une duree  en seconde "))
+    # print(seconde_saisie)
+
+    # VERRIFICATION PAR RAPPORT AU NB SAISIE 
+    if seconde_saisie < 0:
+        print("erreur ton nombre de seconde doit etre positif ")
+    else:
+        # print("Super ton nb est superieur a 0")
+        while seconde_saisie > 0: 
+            print(seconde_saisie)
+            time.sleep(1)
+            seconde_saisie = seconde_saisie -1
+            
+        if seconde_saisie == 0: 
+            print("Fin du compte a rebour")
+           
+
+except Exception as e:
+    print( f"On a une erreur  : {e}")
