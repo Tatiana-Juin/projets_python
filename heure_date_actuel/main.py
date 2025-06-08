8d155bd6c3397ed1d1ee0e24b3f02b971abaef70
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-06-08 20:59:59 +0200

    Creation du projet date heure actuel . Il fonctionne j'ai utiliser datetime

---
+++ heure_date_actuel/main.py
@@ -0,0 +1,7 @@
+import datetime
+#pour recuperer la date et l'heure actuel 
+heure_date_actuel = datetime.datetime.now()
+
+#format de la date 
+format_date= heure_date_actuel.strftime("%d-%m-%Y %H:%M:%S ")
+print(format_date)
