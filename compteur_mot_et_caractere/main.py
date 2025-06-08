ff3f8ac8efd9134d91e837f6804921fb2eca4af1
Tatiana-Juin <username@users.noreply.github.com>
2025-06-08 20:38:53 +0200

    J'ai ajouter les commentaire et enlver ce qui etait inutile

--- compteur_mot_et_caractere/main.py
+++ compteur_mot_et_caractere/main.py
@@ -8,30 +8,27 @@
 
 
 import time
-import os
 
-def clear_console():
-    # Commande pour effacer la console (dépend du système d'exploitation)
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 # VREFICATION QUE L'UTILISATEUR N'A PAS SAISIE AUTRE CHOSE QUE UN NOMBRE  
 
 try:
     seconde_saisie = int(input("Saisie une duree  en seconde "))
-    # print(seconde_saisie)
+
 
     # VERRIFICATION PAR RAPPORT AU NB SAISIE 
     if seconde_saisie < 0:
         print("erreur ton nombre de seconde doit etre positif ")
     else:
-        # print("Super ton nb est superieur a 0")
+        # BOUCLE POUR QUE CELA S'ARRETE S'ARRETE QUAND LE NB EST EGALE A 0
         while seconde_saisie > 0: 
             print(seconde_saisie)
+            # PERMET DE FAIRE UNE PAUSE D'UNE SECONDE AVANT D'AFFICHER LE NB SUIVANT 
             time.sleep(1)
             seconde_saisie = seconde_saisie -1
             
-        if seconde_saisie == 0: 
-            print("Fin du compte a rebour")
+            if seconde_saisie == 0: 
+                print("Fin du compte a rebour")
            
 
 except Exception as e:
