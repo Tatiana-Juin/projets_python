3272b430ff153aa5a6f09d8585803d4470920d58
Tatiana-Juin <username@users.noreply.github.com>
2025-06-16 12:05:21 +0200

    J'ai trouver une solution provisoire pour que chaque entrer soit separer et aille a la ligne dans le projet journal de bord

--- journal_de_bord/main.py
+++ journal_de_bord/main.py
@@ -1,8 +1,10 @@
 
 filename="journal_bord.txt"
 saisi_user = str(input("Ecris dans ton journal de bord ")).strip()
+ajout_text = f"\n {saisi_user}"
 
 with open(filename,'a',encoding='utf-8') as file:
-    file.write(saisi_user)
+    
+    file.write(ajout_text)
 
 print(f"Du texte a etait ajouter au journal de bord {filename}")
