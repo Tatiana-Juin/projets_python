59c744a6c49546d4470a01985f353747fab968bd
Tatiana-Juin <username@users.noreply.github.com>
2025-06-16 11:53:21 +0200

    Nouveau projet journal de bord

--- compte_a_rebourt/main.py
+++ compte_a_rebourt/main.py
@@ -1,9 +1,24 @@
 import time
-import os
 
-def clear_console():
-    # Commande pour effacer la console (dépend du système d'exploitation)
-    os.system('cls' if os.name == 'nt' else 'clear')
+# VREFICATION QUE L'UTILISATEUR N'A PAS SAISIE AUTRE CHOSE QUE UN NOMBRE  
 
-duree_saisie = int(input("Saisie une duree "))
-print(duree_saisie)
+try:
+    seconde_saisie = int(input("Saisie une duree  en seconde "))
+    
+    # VERRIFICATION PAR RAPPORT AU NB SAISIE 
+    if seconde_saisie < 0:
+        print("erreur ton nombre de seconde doit etre positif ")
+    else:
+        
+        while seconde_saisie > 0: 
+            print(seconde_saisie)
+            time.sleep(1)
+            seconde_saisie = seconde_saisie -1
+
+        #pour afficher le message fin du compte a rebourt 
+        if seconde_saisie == 0: 
+            print("Fin du compte a rebour")
+           
+
+except Exception as e:
+    print( f"On a une erreur  : {e} . Il faut saisir un chiffre et rien d'autre.")
