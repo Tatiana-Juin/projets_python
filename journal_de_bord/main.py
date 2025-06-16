5136b2522bfa6077a53dfd4db7222fd64dcd863a
Tatiana-Juin <username@users.noreply.github.com>
2025-06-16 12:06:26 +0200

    Ajout d'un commentaire dans le projet journal de bord

--- journal_de_bord/main.py
+++ journal_de_bord/main.py
@@ -2,7 +2,7 @@
 filename="journal_bord.txt"
 saisi_user = str(input("Ecris dans ton journal de bord ")).strip()
 ajout_text = f"\n {saisi_user}"
-
+#a permet d'ajouter du etxte et de ne pas ecraser els donner quand il a une nouvelle entrer 
 with open(filename,'a',encoding='utf-8') as file:
     
     file.write(ajout_text)
