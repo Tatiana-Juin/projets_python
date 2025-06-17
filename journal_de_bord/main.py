3dc444e5fad56329f4d48b72a172aa64c5b52bca
Tatiana-Juin <username@users.noreply.github.com>
2025-06-17 17:36:07 +0200

    L'utilisateur peut saisir la date à laquel il ajoute du texte et le  texte juste après

--- journal_de_bord/main.py
+++ journal_de_bord/main.py
@@ -1,17 +1,20 @@
 import os 
 
 filename="journal_bord.txt"
+date_saisie = str(input("Saisie la date d'ecriture de ton texte ")).strip()
 saisi_user = str(input("Ecris dans ton journal de bord ")).strip()
 
 #verifie si le fichier existe si ce n'est pas le cas on ne passe pas a la ligne mais si'il existe le texte sera  a  la ligne  
 if not os.path.exists(filename):
-    ajout_text = saisi_user
+    text_journal=f"{date_saisie} : {saisi_user}"
+    
 else:
-    ajout_text = f"\n{saisi_user}"
+    text_journal = f"\n{date_saisie}  : {saisi_user}"
+    
 
 #a permet d'ajouter du etxte et de ne pas ecraser els donner quand il a une nouvelle entrer 
 with open(filename,'a',encoding='utf-8') as file:
-    
-    file.write(ajout_text)
+    file.write(text_journal)
+    # file.write(ajout_text)
 
 print(f"Du texte a etait ajouter au journal de bord {filename}")
