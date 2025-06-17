bd34171cb067aa8c2b459f5fba931d0f8271653d
Tatiana-Juin <username@users.noreply.github.com>
2025-06-17 17:26:58 +0200

    J'ai fait en sorte que le premier texte ajouter lors de la creation du fichier ne passe pas a la ligne mais que les texte qui y sont ajouter eux passe a la ligne

--- journal_de_bord/main.py
+++ journal_de_bord/main.py
@@ -1,7 +1,14 @@
+import os 
 
 filename="journal_bord.txt"
 saisi_user = str(input("Ecris dans ton journal de bord ")).strip()
-ajout_text = f"\n {saisi_user}"
+
+#verifie si le fichier existe si ce n'est pas le cas on ne passe pas a la ligne mais si'il existe le texte sera  a  la ligne  
+if not os.path.exists(filename):
+    ajout_text = saisi_user
+else:
+    ajout_text = f"\n{saisi_user}"
+
 #a permet d'ajouter du etxte et de ne pas ecraser els donner quand il a une nouvelle entrer 
 with open(filename,'a',encoding='utf-8') as file:
     
