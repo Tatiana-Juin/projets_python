54c2bf2949bd7f23ba328ccd5637eadd70810629
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-06-18 11:20:30 +0200

    J'ai fait un projet ou on calculer la moyenne de la classe . J'ai utiliser un fichier json ou il avait les données . J'ai fini le projet

---
+++ note_classe/main.py
@@ -0,0 +1,22 @@
+import json
+# variable pour ajouter la moyenne et le compteur initialiser a 0 
+nombre_eleves = 0
+moyenne = 0;
+
+# nom du fichier 
+filename = "data.json"
+
+# Pour lire le fichier 
+with open(filename,'r',encoding='utf-8') as file:
+      loaded_data=json.load(file)
+
+# Boucle pour parourir les données des eleves 
+for eleve in loaded_data['eleves']:
+    # pour additionner chaque moyenne 
+    moyenne = moyenne + eleve['moyenne']
+    # pour connaitre le nombre de moyenne 
+    nombre_eleves +=1
+# formule pour calculer la moyenne de la classe 
+moyenne_classe = moyenne / nombre_eleves
+
+print(f"La moyenne de la classe est de {moyenne_classe}")
