8a9309fbfe060d2d75e5addeace97eb59fada0b6
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-06-23 11:31:12 +0200

    L'ajout d'un ingredient fonctionne depuis ma saisieutilisateur maintenant je vais faire en sorte que si c'st le premier cela l'ajoute directement sinon cela va verifier que l'ingredient existe pas dejà . Si c'est le cas cela affcihera un message erreur

--- liste_course_avec_bdd/fonction.py
+++ liste_course_avec_bdd/fonction.py
@@ -1,18 +1,27 @@
-# fonction.py
-# FICHIER POUR ECRIRE LES FONCTIONS 
 
-def ajout_produit(cursor, cnx): 
+
+
+
+
+
+
+
+# FICHIER POUR ECRIRE LES FONCTIONS 
+def ajout_produit(cursor, cnx,ingredient_saisie): 
     add_produit = (
         "INSERT INTO produits"
         "(nom_produit)"
         "VALUES (%s)"
     )
     # Ceci DOIT être un tuple avec une virgule
-    data_produit = ("Pomme",) 
+    # data_produit = ("Poire",) 
+    data_produit = (ingredient_saisie,)
+
     
     try:
         cursor.execute(add_produit, data_produit)
         print(f"DEBUG: Exécution réussie pour {data_produit[0]}")
+        
         cnx.commit() # Ceci enregistre les modifications
         print("DEBUG: Modifications validées dans la base de données !")
     except Exception as e:
