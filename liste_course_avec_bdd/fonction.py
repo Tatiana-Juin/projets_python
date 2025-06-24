05cd4b370d527c147f3801ce3a64e8d5dbac7a0d
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-06-24 18:01:25 +0200

    J'ai enlever certains message erreur qui servais a rien et j'en ai modifier certains

--- liste_course_avec_bdd/fonction.py
+++ liste_course_avec_bdd/fonction.py
@@ -22,10 +22,10 @@ def ajout_produit(cursor, cnx,ingredient_saisie):
     
     try:
         cursor.execute(add_produit, data_produit)
-        print(f"DEBUG: Exécution réussie pour {data_produit[0]}")
+        print(f" Exécution réussie pour {data_produit[0]}")
         
         cnx.commit() # Ceci enregistre les modifications
-        print("DEBUG: Modifications validées dans la base de données !")
+        # print("DEBUG: Modifications validées dans la base de données !")
     except Exception as e:
         cnx.rollback() # Annuler si quelque chose ne va pas
         print(f"ERREUR: Échec de l'ajout du produit ou de la validation : {e}")
