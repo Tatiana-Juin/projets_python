67d4e9186d30b2c04dccd553efc81af64b33dd17
Tatiana-Juin <username@users.noreply.github.com>
2025-06-24 17:41:01 +0200

    La fonction de suppression fonctionne. On verifie que le produit existe avant de le supprimer s'il existe aps on le supprime pas on affiche un message erreur

--- liste_course_avec_bdd/fonction.py
+++ liste_course_avec_bdd/fonction.py
@@ -6,7 +6,7 @@
 
 
 
-# FICHIER POUR ECRIRE LES FONCTIONS 
+
 #FONCTION POUR AJOUTER UN INGREDIENT 
 def ajout_produit(cursor, cnx,ingredient_saisie): 
     #REQUETE
@@ -30,7 +30,7 @@ def ajout_produit(cursor, cnx,ingredient_saisie):
         cnx.rollback() # Annuler si quelque chose ne va pas
         print(f"ERREUR: Échec de l'ajout du produit ou de la validation : {e}")
 
-#FONCTION POUR COMPTER LE NB INGREDIENT 
+#FONCTION POUR COMPTER LE NB DE PRODUIS 
 def count_ingredient(cursor):
     cursor.execute("SELECT COUNT(id_produit) as nb_produit FROM produits")
     resultat = cursor.fetchone()
@@ -38,8 +38,25 @@ def count_ingredient(cursor):
     nombre_produits = resultat[0]
     return nombre_produits
 
+# FONCTION POUR SAVOIR SI LE PRODUIT EXISTE DEJA DANS LA BDD 
 def produit_exist(cursor,nom_produit):
     query = "SELECT nom_produit FROM produits WHERE nom_produit= %s"
     cursor.execute(query,(nom_produit,))
     resultat = cursor.fetchone()
     return  resultat is not None
+
+
+# FONCTION POUR SUPPRIMER UN PRODUIT 
+def delete_produit(cursor,cnx,nom_produit_supprimer):
+    sql = "DELETE FROM produits WHERE nom_produit = %s "
+    cursor.execute(sql,(nom_produit_supprimer,))
+    cnx.commit()
+    print("Supprimer")
+
+# POUR VOIR TOUS LES PRODUIT QUI SONT PRESENT DANS LA BDD
+def show_produit(cursor):
+    sql = "SELECT nom_produit FROM produits"
+    # quand on utilise execute et qu'il n'y a pas de parametre autre que cursor on utilise une chaine de caractere 
+    cursor.execute(sql)
+    resultat = cursor.fetchall()
+    return resultat
--- liste_course_avec_bdd/main.py
+++ liste_course_avec_bdd/main.py
@@ -10,7 +10,7 @@ try:
     print("Connexion à la base de données réussie !")
     cursor = cnx.cursor()
 
-    nb_ingredient = 0;
+    # nb_ingredient = 0;
 
     # MENU
     print("Tu dois taper :");
@@ -18,30 +18,56 @@ try:
     print("2 - Supprimer un ingredient");
     print("3 - Quitter l'application");
     
-    nb_saisie = int(input("Quel est ton nombre"));
+    nb_saisie = int(input("Quel est ton nombre ? "));
 
     while nb_saisie != 3:
         # AJOUT
         if nb_saisie == 1:
              # POUR SAISIR UN INGREDIENT
             nb_produit = fonction.count_ingredient(cursor)
-            #aucun ingredient
+
+            #aucun ingredient on peut ajouter l'ingredient
             if nb_produit == 0:
                 ingredient_saisie = str(input("Quel est ton ingredient ?  ")).lower().strip()
                 fonction.ajout_produit(cursor, cnx,ingredient_saisie)
-               
+            
+            #verifie que l'ingredient n'existe pas dans la bdd     
             else:
-                print("plusieurs")
                 ingredient_saisie = str(input("Quel est ton ingredient ?  ")).lower().strip()
                 existe = fonction.produit_exist(cursor,ingredient_saisie)
-                
+
+                # condition pour savoir si l'ingredient est deja present dans la liste des course ou si on peut l'ajouter 
                 if(existe == True):
-                    print("Cela existe")
+                    print("L'ingredient est éjà présent dans ta liste de course tu ne peut pas l'ajouter")
                 else:
-                    print("Cela n'existe pas ")
+                    # print("Cela n'existe pas ")
+                    fonction.ajout_produit(cursor, cnx,ingredient_saisie)
         elif nb_saisie ==2:
-            nb_saisie = int(input("Quel est ton nombre"));
-            print("Supprimer")
+
+            # fonction pour savoir le nb de produit dans la bdd 
+            nb_produit = fonction.count_ingredient(cursor)
+
+            # Si il a aucun produit on a un message 
+            if nb_produit ==0:
+                print("Tu ne peut pas supprimer de produit car il en a pas dans labase de donnée")
+            else:
+                nom_produit_supprimer = str(input("Quel est le produit que tu veux supprimer ? ")).lower().strip()
+               
+            #    Appelle de la fonction pour sazvoir si le produit existe 
+                existe = fonction.produit_exist(cursor,nom_produit_supprimer)
+                if existe == True : 
+                    # print("Le produit existe ")
+                    supprimer_produit = fonction.delete_produit(cursor,cnx,nom_produit_supprimer)
+
+                    affiche_produit = fonction.show_produit(cursor)
+                    for ligne in affiche_produit:
+                        print("La liste des produits : ")
+                        print(ligne[0])
+
+                    
+                else:
+                    print("Tu ne peut pas supprimer un produit qui n'existe pas dans la base de données ")
+                    
 
 
         print("Tu dois taper :");
@@ -49,7 +75,7 @@ try:
         print("2 - Supprimer un ingredient");
         print("3 - Quitter l'application");
     
-        nb_saisie = int(input("Quel est ton nombre"));
+        nb_saisie = int(input("Quel est ton nombre ? "));
 
     
 
