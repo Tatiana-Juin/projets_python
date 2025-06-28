2f4c3a9a543e12aca087c3e75f6bfcc5e386e088
Tatiana-Juin <username@users.noreply.github.com>
2025-06-28 11:22:16 +0200

    J'ai ajouter un champs de saisie pour l'utilisateur . Pour l'instant il ne fonctionne. Je veux simplement le visuel pour l'instant

--- liste_tache/main.py
+++ liste_tache/main.py
@@ -9,6 +9,13 @@ frame = Frame(window,bg="#ffeeee")
 
 label_titre = Label(frame,text="Liste des taches",font=("Helvetica",20), bg="#ffeeee",fg="red")
 label_titre.pack()
+
+#Pour que l'utilisateur saisie une tache 
+tache_entry = Entry(frame,font=("Helvetica",17),fg="black")
+tache_entry.pack(pady=(10,0))
+
+
+
 #pady => marge => haut et bas 
 frame.pack(pady=(20,0))
 
