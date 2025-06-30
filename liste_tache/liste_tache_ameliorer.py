6d67936958f4f20c74e8c4496828c5ef4dfa8378
Tatiana-Juin <username@users.noreply.github.com>
2025-06-30 11:42:05 +0200

    On peut ajouter une task la voir s'afficher ainsi que de voir un bouton supprimer a coter . Il me reste que a faire la fonction supprimer pour supprimer la task

--- liste_tache/liste_tache_ameliorer.py
+++ liste_tache/liste_tache_ameliorer.py
@@ -1,5 +1,22 @@
 from tkinter import *
 
+# tableau pour la liste des taches 
+tab_task=[]
+
+def add_task(task_entry):
+    # curent_row = 0
+    task = task_entry.get()
+    if task.strip():
+        tab_task.append(task)
+        task_entry.delete(0, END)
+        # On fait cette ligne pour incrementer la row en recuperant l'index . start = 1 dit que la boucle commence a 1 fonction de enumerate
+        for index,task in enumerate(tab_task,start=1):
+            label_task = Label(frame,text=task,font=("Arial",15),bg="black",fg="white")
+            label_task.grid(row=index,column=0,padx=5,pady=5)
+            # Pour le bouton supprimer 
+            btn_delete = Button(frame,text="Supprimer",font=("Arial",12),bg="white",fg="black")
+            btn_delete.grid(row=index,column = 1,padx=5,pady=5)
+
 # CONFIGURATION DE LA PREMIERE FENETRE 
 window = Tk()
 window.title("Application de liste des taches ")
@@ -20,7 +37,7 @@ task = Entry(frame,font=("Arial",15),bg="white",fg="black")
 task.grid(row=0,column=0,padx=5,pady=5)
 
 
-btn_add_task = Button(frame,text="Ajout",font=("Arial",13), bg="white",fg="black")
+btn_add_task = Button(frame,text="Ajout",font=("Arial",13), bg="white",fg="black", command=lambda:add_task(task))
 btn_add_task.grid(row=0,column=1,padx=5,pady=5)
 
 
