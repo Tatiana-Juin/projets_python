7ce49fcc3d379f2ff6d58ceaa11ce3e43499f745
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-07-24 23:49:26 +0200

    Je vais refaire le projet de compteur de mot

--- compteur_mot_et_caractere/main.py
+++ compteur_mot_et_caractere/main.py
@@ -7,29 +7,29 @@
 
 
 
-import time
+# import time
 
 
 # VREFICATION QUE L'UTILISATEUR N'A PAS SAISIE AUTRE CHOSE QUE UN NOMBRE  
 
-try:
-    seconde_saisie = int(input("Saisie une duree  en seconde "))
+# try:
+#     seconde_saisie = int(input("Saisie une duree  en seconde "))
 
 
     # VERRIFICATION PAR RAPPORT AU NB SAISIE 
-    if seconde_saisie < 0:
-        print("erreur ton nombre de seconde doit etre positif ")
-    else:
+    # if seconde_saisie < 0:
+    #     print("erreur ton nombre de seconde doit etre positif ")
+    # else:
         # BOUCLE POUR QUE CELA S'ARRETE S'ARRETE QUAND LE NB EST EGALE A 0
-        while seconde_saisie > 0: 
-            print(seconde_saisie)
+        # while seconde_saisie > 0: 
+        #     print(seconde_saisie)
             # PERMET DE FAIRE UNE PAUSE D'UNE SECONDE AVANT D'AFFICHER LE NB SUIVANT 
-            time.sleep(1)
-            seconde_saisie = seconde_saisie -1
+            # time.sleep(1)
+            # seconde_saisie = seconde_saisie -1
             
-            if seconde_saisie == 0: 
-                print("Fin du compte a rebour")
+            # if seconde_saisie == 0: 
+            #     print("Fin du compte a rebour")
            
 
-except Exception as e:
-    print( f"On a une erreur  : {e}")
+# except Exception as e:
+#     print( f"On a une erreur  : {e}")
